51481e4d9823aee27c048d60b4215488854ac026
Paul Jung <pbnjcub@gmail.com>
2022-06-27 13:20:08 -0400

    commit tree 17c47d706b345aa2287873f310c0259e4336e9e6

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 34;
+let num4 = 30;
+
+let mod = num3 % num4;
+
+
+
+
+let max = Math.max(1, 2, 3, 4, 20);
+
+
+
+
+
